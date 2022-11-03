f3567eacda4a292ba919df4edb5ccc2b170526cb
aalred <aalex.red1@gmail.com>
2022-11-02 18:36:30 -0600

    updated files

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,30 +9,28 @@ import Button from '@mui/material/Button'
 
 const Home: NextPage = () => {
 
- function handleClick() {
-   const element = window?.document.getElementById('component');
-   element?.scrollIntoView();
+  function handleClick() {
+    const element = window?.document.getElementById('component');
+    element?.scrollIntoView();
   };
 
   return (
     <div className={styles.container}>
-      <div className={styles['container__shadow']}>
-        <Layout>
-          <div className={styles['container__text']}>
-            <h2> Hello I&apos;m {"< Josue Rojas />"} <br /> Full Stack Web Developer </h2>
+      <Layout>
+        <div className={styles['container__text']}>
+          <h2> Hello I&apos;m {"< Josue Rojas />"} <br /> Full Stack Web Developer </h2>
+        </div>
+        <div className={styles['container__items']} id='component' >
+          <Button className={styles['container__items-btn']} onClick={() => handleClick()} variant="text" color="inherit">
+            See my projects
+          </Button>
+          <div className={styles['container__items-component']}>
+            <Projects />
           </div>
-          <div className={styles['container__items']} id='component' >
-            <Button className={styles['container__items-btn']} onClick={() => handleClick()} variant="text" color="inherit">
-              See my projects
-            </Button>
-            <div className={styles['container__items-component']}>
-              <Projects />
-            </div>
-            <br />
-            <br />
-          </div>
-        </Layout >
-      </div>
+          <br />
+          <br />
+        </div>
+      </Layout >
     </div>
   );
 }
